646f46bb254f7608e2a8aa51875fbaf3b2b23080
Ethan <ethancr8n@gmail.com>
2024-10-17 01:31:15 -0700

    Update

---
+++ PassingEnums/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week:");
+                string userInput = Console.ReadLine();
+
+                DaysOfWeek currentDay = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), userInput, true);
+
+                Console.WriteLine("You entered: " + currentDay);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+        }
+    }
+}
