57e93232a32da32c6cbabb8e2aecada77414b746
Ethan <ethancr8n@gmail.com>
2024-10-17 01:26:53 -0700

    Update

---
+++ Polymorphism/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Employee : IQuittable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void Quit()
+        {
+            Console.WriteLine($"{FirstName} {LastName} has quit the job.");
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee { FirstName = "John", LastName = "Doe" };
+
+            IQuittable quittable = employee;
+
+            quittable.Quit();
+        }
+    }
+}
