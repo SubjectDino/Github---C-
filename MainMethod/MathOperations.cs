454788871ff75a04095c5476d3141e712f9ccf4f
Ethan <ethancr8n@gmail.com>
2024-10-17 01:13:31 -0700

    Update

---
+++ MainMethod/MathOperations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class MathOperations
+    {
+        // Method for integer - adds 10 to the input integer
+        public int Calculate(int number)
+        {
+            return number + 10;
+        }
+
+        // Method for decimal - multiplies the input decimal by 2 and returns the integer value
+        public int Calculate(decimal number)
+        {
+            return (int)(number * 2);
+        }
+
+        // Method for string - tries to convert the string to an integer, then subtracts 5
+        public int Calculate(string number)
+        {
+            int parsedNumber;
+            bool isInteger = int.TryParse(number, out parsedNumber);
+
+            if (isInteger)
+            {
+                return parsedNumber - 5;
+            }
+            else
+            {
+                throw new ArgumentException("Input string was not in the correct format.");
+            }
+        }
+    }
+}
---
+++ MainMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations mathOps = new MathOperations();
+
+            int result1 = mathOps.Calculate(10);
+            Console.WriteLine("Result of integer calculation (10 + 10): " + result1);
+
+            int result2 = mathOps.Calculate(12.5m);
+            Console.WriteLine("Result of decimal calculation (12.5 * 2): " + result2);
+
+            int result3 = mathOps.Calculate("20");
+            Console.WriteLine("Result of string calculation ('20' - 5): " + result3);
+        }
+    }
+}
