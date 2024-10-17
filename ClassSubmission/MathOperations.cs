1f8e6660772901ee7b8038da58e0e3efe5016aaa
Ethan <ethancr8n@gmail.com>
2024-10-17 01:23:15 -0700

    Update

---
+++ ClassSubmission/MathOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    public static class MathOperations
+    {
+        public static void DivideByTwo(int number, out int result)
+        {
+            result = number / 2;
+        }
+
+        public static void AddNumbers(int number1, int number2, out int sum)
+        {
+            sum = number1 + number2;
+        }
+
+        public static int AddNumbers(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public static decimal AddNumbers(decimal number1, decimal number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
---
+++ ClassSubmission/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to divide by 2:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int dividedResult;
+            MathOperations.DivideByTwo(userInput, out dividedResult);
+            Console.WriteLine("Your number divided by 2 is: " + dividedResult);
+
+            Console.WriteLine("\nEnter two numbers to add:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int sumResult;
+            MathOperations.AddNumbers(num1, num2, out sumResult);
+            Console.WriteLine($"The sum of {num1} and {num2} is: {sumResult}");
+
+            Console.WriteLine("\nOverloaded methods:");
+            int intSum = MathOperations.AddNumbers(5, 10);
+            Console.WriteLine("Sum of 5 and 10 (int): " + intSum);
+
+            decimal decimalSum = MathOperations.AddNumbers(5.5m, 10.5m);
+            Console.WriteLine("Sum of 5.5 and 10.5 (decimal): " + decimalSum);
+        }
+    }
