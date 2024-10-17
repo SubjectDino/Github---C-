6f5d140e06470035c96cb21a08da52e6dd85b5fc
Ethan <ethancr8n@gmail.com>
2024-10-17 01:29:17 -0700

    Updates

---
+++ OperatorsSubmission/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            if (ReferenceEquals(emp1, emp2))
+            {
+                return true;
+            }
+
+            if ((object)emp1 == null || (object)emp2 == null)
+            {
+                return false;
+            }
+
+            return emp1.Id == emp2.Id;
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1 == emp2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Employee employee = (Employee)obj;
+            return Id == employee.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ OperatorsSubmission/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee { Id = 1, FirstName = "John", LastName = "Doe" };
+            Employee employee2 = new Employee { Id = 2, FirstName = "Jane", LastName = "Smith" };
+
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("Both employees have the same Id.");
+            }
+            else
+            {
+                Console.WriteLine("The employees have different Ids.");
+            }
+
+            employee2.Id = 1;
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("Now both employees have the same Id.");
+            }
+            else
+            {
+                Console.WriteLine("The employees still have different Ids.");
+            }
+        }
+    }
+}
