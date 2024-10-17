83c803f2632d816a5974c0489784d747570d9964
Ethan <ethancr8n@gmail.com>
2024-10-17 00:54:44 -0700

    Parameters

---
+++ Parameters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employeeString = new Employee<string>();
+            employeeString.Things = new List<string> { "John", "Doe", "Jane" };
+
+            Employee<int> employeeInt = new Employee<int>();
+            employeeInt.Things = new List<int> { 100, 200, 300 };
+
+            Console.WriteLine("String List:");
+            foreach (var thing in employeeString.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.WriteLine("\nInteger List:");
+            foreach (var thing in employeeInt.Things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+    }
+}
