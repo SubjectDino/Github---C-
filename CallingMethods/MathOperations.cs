6ff410ea08571699871a97e4952dd4d4de073538
Ethan <ethancr8n@gmail.com>
2024-10-17 00:45:36 -0700

    Update

---
+++ CallingMethods/MathOperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    using System;
+    public class MathOperations
+    {
+        public int AddTen(int number)
+        {
+            return number + 10;
+        }
+        public int MultiplyByTwo(int number)
+        {
+            return number * 2;
+        }
+        public int SubtractFive(int number)
+        {
+            return number - 5;
+        }
+    }
+}
---
+++ CallingMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for a number
+            Console.WriteLine("Enter a number to perform math operations on:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // Create an instance of the MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Call the AddTen method and display the result
+            int result1 = mathOps.AddTen(userInput);
+            Console.WriteLine("After adding 10: " + result1);
+
+            // Call the MultiplyByTwo method and display the result
+            int result2 = mathOps.MultiplyByTwo(userInput);
+            Console.WriteLine("After multiplying by 2: " + result2);
+
+            // Call the SubtractFive method and display the result
+            int result3 = mathOps.SubtractFive(userInput);
+            Console.WriteLine("After subtracting 5: " + result3);
+        }
+    }
+}
