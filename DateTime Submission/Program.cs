be95b1d29210e13e49c85467f26e8667a6a454dd
Ethan <ethancr8n@gmail.com>
2024-10-17 21:18:44 -0700

    Update

---
+++ DateTime Submission/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine("Current date and time: " + currentDateTime);
+
+            Console.WriteLine("Please enter a number of hours:");
+            int hours = int.Parse(Console.ReadLine());
+
+            DateTime futureDateTime = currentDateTime.AddHours(hours);
+            Console.WriteLine("In " + hours + " hours, the date and time will be: " + futureDateTime);
+        }
+    }
+}
+}
